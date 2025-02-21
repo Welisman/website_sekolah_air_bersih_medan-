9c429842c1bdcb0597df674432912219c3a2d2d5
NVYman <w.pentos@gmail.com>
2025-02-22 01:03:54 +0700

    Memperbarui back-end/index.js

--- back-end/index.js
+++ back-end/index.js
@@ -15,6 +15,7 @@ const kataSambutanRoutes = require("./src/routes/kata_sambutan_route");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/api/kata-sambutan", kataSambutanRoutes);
 
 app.use(cors({
     origin: "http://localhost:5173",
@@ -25,13 +26,3 @@ app.use('/admin', adminRoutes)
 app.listen(PORT, () => {
     console.log(`Server berjalan pada http://localhost:${PORT}`)
 })
-
-app.use(cors());
-app.use(express.json());
-
-// Rute CRUD Kata Sambutan
-app.use("/api/kata-sambutan", kataSambutanRoutes);
-
-app.listen(3001, () => {
-    console.log("Server berjalan di http://localhost:3001");
-});
